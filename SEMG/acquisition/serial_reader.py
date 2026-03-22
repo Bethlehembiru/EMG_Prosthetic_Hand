3de2053ec2a4679562147e33ba84ec49e84fd7aa
ortir <ortirlukman@gmail.com>
2026-03-22 11:40:10 +0300

    Update serial_reader.py

--- SEMG/acquisition/serial_reader.py
+++ SEMG/acquisition/serial_reader.py
@@ -1,1 +1,45 @@
+import serial
+import time
 
+
+class EMGSerialReader:
+
+    def __init__(self, port='COM3', baudrate=115200):
+
+        self.ser = serial.Serial(port, baudrate, timeout=1)
+        time.sleep(2)
+
+        self._initialize()
+
+    def _initialize(self):
+
+        print("Waiting for Arduino...")
+
+        while True:
+            line = self.ser.readline().decode().strip()
+
+            if line == "READY":
+                print("Arduino READY")
+                break
+
+        self.ser.write(b"START\n")
+
+        while True:
+            line = self.ser.readline().decode().strip()
+
+            if line == "STREAMING":
+                print("Streaming started")
+                break
+
+    def read_sample(self):
+
+        try:
+            line = self.ser.readline().decode().strip()
+
+            if line.isdigit():
+                return int(line)
+            else:
+                return None
+
+        except:
+            return None
